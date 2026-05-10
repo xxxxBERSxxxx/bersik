1a21c7b252ec860c29befd9234e02504cc6ce04c
Bers <xxxxbersxxxx@mail.ru>
2026-05-10 12:25:49 +0300

    2

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
+import type { NextConfig } from "next";
 
-const nextConfig = {
-  experimental: {
-    optimizeCss: true,
+const nextConfig: NextConfig = {
+  // Отключаем встроенную оптимизацию изображений, так как Netlify не поддерживает её "из коробки"
+  // без дополнительных танцев с бубном. Картинки будут отдаваться как есть.
+  images: {
+    unoptimized: true,
   },
+  // Эта настройка важна для Netlify. Она говорит Next.js собирать проект
+  // в автономный пакет, который Netlify-плагин развернет правильно.
+  output: 'standalone',
 };
 
-module.exports = {
- output: 'standalone', 
-  distDir: '.next',
-  images: {
-    unoptimized: true, // Отключает серверную оптимизацию изображений
-  },
-}
+export default nextConfig;
